e13ae274e87646b4cca5e176f7dad99b00efdc9b
Denis Molodtsov <denis-ne@yandex.ru>
2021-09-11 19:08:59 -0400

    first commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+const config = {
+    cosmosDb: {
+        connectionString: ""
+    }
+};
+
+export default config
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Simplest example of posting a message to a topic
+import config from './config';
+
+
+console.log("hello from TypeScript");
